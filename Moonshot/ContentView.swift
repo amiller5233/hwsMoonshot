4a1e54dde342aee3377964d3bb875a140840bf74
amiller5233@gmail.com <amiller5233@gmail.com>
2023-07-19 18:42:13 -0700

    add astronauts to mission view

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
                         NavigationLink {
-                            /*@START_MENU_TOKEN@*/Text(mission.description)/*@END_MENU_TOKEN@*/
+                            MissionView(mission: mission, astronauts: astronauts)
                         } label: {
                             VStack {
                                 Image(mission.image)
