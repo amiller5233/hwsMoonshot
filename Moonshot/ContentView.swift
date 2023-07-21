2f2eab0bc3a6bb75d6e899278b6f95ae79de3ea4
amiller5233@gmail.com <amiller5233@gmail.com>
2023-07-20 22:06:02 -0700

    add list view toggle

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -11,14 +11,33 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    @State private var gridView = true
+    
     var body: some View {
         NavigationView {
-            ScrollView {
-                MissionGridView(astronauts: astronauts, missions: missions)
+            Group {
+                if gridView {
+                    MissionGridView(astronauts: astronauts, missions: missions)
+                } else {
+                    MissionListView(astronauts: astronauts, missions: missions)
+                }
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button() {
+                        gridView.toggle()
+                    } label: {
+                        if gridView {
+                            Image(systemName: "list.bullet")
+                        } else {
+                            Image(systemName: "square.grid.2x2")
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -27,25 +46,67 @@ struct MissionGridView: View {
     let astronauts: [String: Astronaut]
     let missions: [Mission]
     
-    
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
     
     var body: some View {
-        LazyVGrid(columns: columns) {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundColor(.white.opacity(0.5))
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+struct MissionListView: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    
+    var body: some View {
+        List {
             ForEach(missions) { mission in
                 NavigationLink {
                     MissionView(mission: mission, astronauts: astronauts)
                 } label: {
-                    VStack {
+                    HStack(spacing: 15) {
                         Image(mission.image)
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .padding()
+                            .frame(width: 50, height: 50)
                         
-                        VStack {
+                        VStack(alignment: .leading) {
                             Text(mission.displayName)
                                 .font(.headline)
                                 .foregroundColor(.white)
@@ -54,19 +115,15 @@ struct MissionGridView: View {
                                 .font(.caption)
                                 .foregroundColor(.white.opacity(0.5))
                         }
-                        .padding(.vertical)
-                        .frame(maxWidth: .infinity)
-                        .background(.lightBackground)
                     }
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(.lightBackground)
-                    )
+                    .alignmentGuide(.listRowSeparatorLeading) { _ in
+                        return 0
+                    }
                 }
             }
+            .listRowBackground(Color.darkBackground)
         }
-        .padding([.horizontal, .bottom])
+        .listStyle(.plain)
     }
 }
 
